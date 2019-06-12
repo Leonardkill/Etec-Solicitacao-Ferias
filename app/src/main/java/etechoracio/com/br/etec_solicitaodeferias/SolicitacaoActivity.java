1f6a2d0564ee8ee047e5c7987504b307e70f09ac
Jacob <leonardofaria_7@hotmail.com>
2019-06-11 22:30:00 -0300

    Subindo arquivos 11/06

---
+++ app/src/main/java/etechoracio/com/br/etec_solicitaodeferias/SolicitacaoActivity.java
@@ -0,0 +1,13 @@
+package etechoracio.com.br.etec_solicitaodeferias;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SolicitacaoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_solicitacao);
+    }
+}
